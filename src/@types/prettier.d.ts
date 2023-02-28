2f4da7423e7d2744db199d238815a5c759a1dcc6
isamu tajima <136.tajima@gmail.com>
2023-02-28 22:43:35 +0900

    fix build error

--- src/@types/prettier.d.ts
+++ src/@types/prettier.d.ts
@@ -1,4 +1,4 @@
-declare module 'prettier-plugin-cadence'{
+declare module "prettier-plugin-cadence" {
   export const languages: {
     name: string;
     parsers: string[];
@@ -6,14 +6,16 @@ declare module 'prettier-plugin-cadence'{
   }[];
   export const parsers: {
     cadence: {
-        parse: (text: any) => {
+      parse: (text: any) =>
+        | {
             nodeType: string;
             start: any;
             end: any;
             lineStart: any;
             lineEnd: any;
             value: any;
-        } | {
+          }
+        | {
             children: any[];
             nodeType: string;
             start: any;
@@ -21,23 +23,23 @@ declare module 'prettier-plugin-cadence'{
             lineStart: any;
             lineEnd: any;
             value?: undefined;
-        };
-        astFormat: string;
-        locStart: (node: any) => any;
-        locEnd: (node: any) => any;
+          };
+      astFormat: string;
+      locStart: (node: any) => any;
+      locEnd: (node: any) => any;
     };
   };
   export const printers: {
     cadence: {
-        print: (path: any, options: any, print: any) => any;
-        printComment: (path: any, options: any) => any;
-        canAttachComment: (node: any) => any;
-        getCommentChildNodes: (node: any) => any;
-        handleComments: {};
+      print: (path: any, options: any, print: any) => any;
+      printComment: (path: any, options: any) => any;
+      canAttachComment: (node: any) => any;
+      getCommentChildNodes: (node: any) => any;
+      handleComments: {};
     };
   };
   export const options: {};
   export const defaultOptions: {
     tabWidth: number;
-  }
+  };
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
